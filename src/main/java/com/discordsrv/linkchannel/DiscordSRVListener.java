52ecfa61d3cc3a64089990604a54bff4cad06b43
Mike5357 <mike9452@gmail.com>
2024-01-29 12:41:40 -0500

    Atempt parsing timestamps, Update DiscordSRV

--- src/main/java/com/discordsrv/linkchannel/DiscordSRVListener.java
+++ src/main/java/com/discordsrv/linkchannel/DiscordSRVListener.java
@@ -1,14 +1,21 @@
 package com.discordsrv.linkchannel;
 
 import github.scarsz.discordsrv.DiscordSRV;
-import github.scarsz.discordsrv.api.ListenerPriority;
 import github.scarsz.discordsrv.api.Subscribe;
 import github.scarsz.discordsrv.api.events.*;
+import github.scarsz.discordsrv.dependencies.jda.api.entities.Role;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.TextChannel;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.User;
+import github.scarsz.discordsrv.dependencies.kyori.adventure.text.Component;
 import github.scarsz.discordsrv.util.DiscordUtil;
+
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DiscordSRVListener {
 
@@ -20,38 +27,17 @@ public class DiscordSRVListener {
 
     @Subscribe
     public void discordReadyEvent(DiscordReadyEvent event) {
-        // Example of using JDA's events
-        // We need to wait until DiscordSRV has initialized JDA, thus we're doing this inside DiscordReadyEvent
         DiscordUtil.getJda().addEventListener(new JDAListener(plugin));
 
-        // ... we can also do anything other than listen for events with JDA now,
         plugin.getLogger().info("Chatting on Discord with " + DiscordUtil.getJda().getUsers().size() + " users!");
-        // see https://ci.dv8tion.net/job/JDA/javadoc/ for JDA's javadoc
-        // see https://github.com/DV8FromTheWorld/JDA/wiki for JDA's wiki
     }
-
-    /*@Subscribe(priority = ListenerPriority.MONITOR)
-    public void discordMessageReceived(DiscordGuildMessageReceivedEvent event) {
-        // Example of logging a message sent in Discord
-
-        plugin.getLogger().info("Received a chat message on Discord: " + event.getMessage());
-    }
-
-    @Subscribe(priority = ListenerPriority.MONITOR)
-    public void aMessageWasSentInADiscordGuildByTheBot(DiscordGuildMessageSentEvent event) {
-        // Example of logging a message sent in Minecraft (being sent to Discord)
-
-        plugin.getLogger().info("A message was sent to Discord: " + event.getMessage());
-    }*/
 
     @Subscribe
     public void accountLinked(AccountLinkedEvent event) {
-        // Example of broadcasting a message when a new account link has been made
         User user = DiscordUtil.getJda().getUserById(event.getUser().getId());
         user.openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage("Successfully linked with " + event.getPlayer().getName() + "!").queue());
         Bukkit.broadcastMessage(DiscordSRV.config().getString("LinkingMessageLinkedInGame").replace("%player%",event.getPlayer().getName()));
 
-        // Example of sending a message to a channel called "unlinks" (defined in the config.yml using the Channels option) when a user unlinks
         TextChannel textChannel = DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName("links");
 
         // null if the channel isn't specified in the config.yml
@@ -64,7 +50,6 @@ public class DiscordSRVListener {
 
     @Subscribe
     public void accountUnlinked(AccountUnlinkedEvent event) {
-        // Example of DM:ing user on unlink
         User user = DiscordUtil.getJda().getUserById(event.getDiscordId());
 
         // will be null if the bot isn't in a Discord server with the user (eg. they left the main Discord server)
@@ -72,9 +57,17 @@ public class DiscordSRVListener {
             // opens/retrieves the private channel for the user & sends a message to it (if retrieving the private channel was successful)
             user.openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage("Your account has been unlinked from " + event.getPlayer().getName() + ".").queue());
             Bukkit.broadcastMessage(DiscordSRV.config().getString("LinkingMessageUnlinkedInGame").replace("%player%",event.getPlayer().getName()));
+
+            //remove linked role when unlinked
+            Role linkedRole = DiscordUtil.getRole(DiscordSRV.config().getString("MinecraftDiscordAccountLinkedRoleNameToAddUserTo"));
+            if (linkedRole != null) {
+                DiscordUtil.removeRolesFromMember(DiscordUtil.getMemberById(user.getId()),linkedRole);
+            } else {
+                plugin.getLogger().warning("Linked role not found in config; cannot remove role on unlink.");
+            }
         }
 
-        // Example of sending a message to a channel called "unlinks" (defined in the config.yml using the Channels option) when a user unlinks
+        // sending a message to a channel called "unlinks" (defined in the config.yml using the Channels option) when a user unlinks
         TextChannel textChannel = DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName("unlinks");
 
         // null if the channel isn't specified in the config.yml
@@ -85,13 +78,63 @@ public class DiscordSRVListener {
         }
     }
 
-    /*
     @Subscribe
     public void discordMessageProcessed(DiscordGuildMessagePostProcessEvent event) {
-        // Example of modifying a Discord -> Minecraft message
-
-        event.setProcessedMessage(event.getProcessedMessage().replace("cat", "dog")); // dogs are superior to cats, obviously
+        Component message = event.getMinecraftMessage();
+        event.setMinecraftMessage(parseTimestamps(message));
     }
-    */
+
+    public Component parseTimestamps(Component message) {
+        Pattern pattern = Pattern.compile("<t:\\d*:R>");
+        Matcher matcher = pattern.matcher(message.toString());
+        while(matcher.find()) {
+            System.out.println("Matched timestamp regex");
+            String timestamp = matcher.group().substring(3,13); // trim the timestamp to just the numbers
+
+            Date timestampDate = new Date(Long.parseLong(timestamp) * 1000);
+            String timeUntil = String.valueOf(Duration.between(Instant.now(), timestampDate.toInstant()).toMinutes());
+
+            System.out.println("Parsed minutes: " + timeUntil);
+            if (Integer.parseInt(timeUntil) < 0) {
+                System.out.println("<0");
+                message = message.replaceText(component -> {
+                    component.match(pattern);
+                    component.replacement("[" + timeUntil.substring(1) + " minutes ago]");
+                    component.once();
+                });
+            } else if (Integer.parseInt(timeUntil) == 0) {
+                System.out.println("==0");
+                message = message.replaceText(component -> {
+                    component.match(pattern);
+                    component.replacement("[now]");
+                    component.once();
+                });
+            } else if (Integer.parseInt(timeUntil) == 1) {
+                System.out.println("==1");
+                message = message.replaceText(component -> {
+                    component.match(pattern);
+                    component.replacement("[in 1 minute]");
+                    component.once();
+                });
+            } else {
+                System.out.println(">0");
+                message = message.replaceText(component -> {
+                    component.match(pattern);
+                    component.replacement("[in " + timeUntil + " minutes]");
+                    component.once();
+                });
+            }
+        }
+        return message;
+    }
+
+    @Subscribe
+    public void gameMessageProcessed(GameChatMessagePreProcessEvent event) {
+        //event.setCancelled(true);
+        //System.out.println("Game chat message received: " + event.getMessageComponent());
+        event.setMessageComponent(parseTimestamps(event.getMessageComponent()));
+        //event.setCancelled(false);
+    }
+
 
 }
--- src/main/java/com/discordsrv/linkchannel/Plugin.java
+++ src/main/java/com/discordsrv/linkchannel/Plugin.java
@@ -15,23 +15,6 @@ public class Plugin extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
 
-    /*@EventHandler(priority = EventPriority.NORMAL)
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        String discordId = DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(event.getPlayer().getUniqueId());
-        if (discordId == null) {
-            event.getPlayer().sendMessage(ChatColor.RED + "You are not linked");
-            return;
-        }
-
-        User user = DiscordUtil.getJda().getUserById(discordId);
-        if (user == null) {
-            event.getPlayer().sendMessage(ChatColor.YELLOW + "Couldn't find the user you're linked to");
-            return;
-        }
-
-        event.getPlayer().sendMessage(ChatColor.GREEN + "You're linked to " + user.getAsTag());
-    }*/
-
     @Override
     public void onDisable() {
         DiscordSRV.api.unsubscribe(discordsrvListener);
